d13f0c414626eb2bc720198c56e39fd7972551bb
Mansilla Valentin <avmansillacba@gmail.com>
2025-04-07 18:57:22 -0300

    Proyecto vacio

---
+++ src/main/java/com/todotren/todotren/ToDoTrenApplication.java
@@ -0,0 +1,13 @@
+package com.todotren.todotren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoTrenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoTrenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/todotren/todotren/ToDoTrenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todotren.todotren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoTrenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
