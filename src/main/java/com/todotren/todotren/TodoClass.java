e4dc1c0f5818fd6de31791dfe5a7534572f3c078
Valentin_Mansilla <avmansillacba@gmail.com>
2025-06-17 23:18:53 -0300

    nothing

--- src/main/java/com/todotren/todotren/TodoClass.java
+++ src/main/java/com/todotren/todotren/TodoClass.java
@@ -5,6 +5,6 @@ public class TodoClass {
 
     //tod task controller: cambiar getbyname por un getbyid.
     //tod task controller: simplificar el save para que sea una actualizacion o nuevo.
-    //todo task controller: agregar un deletebyid.
+    //tod task controller: agregar un deletebyid.
     //todo task controller: documentar metodos.
 }
